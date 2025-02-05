7b91afcd0e5b34c32bae874c9fa4879c2483e843
Samuel Bradshaw <sam@Samuels-MacBook-Pro.local>
2025-02-05 12:39:37 -0700

    added the rest of pictures needed, Finalized tab par and pages

---
+++ projects/All About Me/All About Me/BioScreen.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  All About Me
+//
+//  Created by Samuel Bradshaw  on 2/3/25.
+//
+
+import UIKit
+
+class BioScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
