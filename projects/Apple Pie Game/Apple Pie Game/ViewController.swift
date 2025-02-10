e766b730cb24936b02e870fa50c291fc6936c429
Samuel Bradshaw <sam@Samuels-MacBook-Pro.local>
2025-02-10 10:41:26 -0700

    added buttons and images to apple pie. added user interface to update  images and labels.

--- projects/Apple Pie Game/Apple Pie Game/ViewController.swift
+++ projects/Apple Pie Game/Apple Pie Game/ViewController.swift
@@ -8,12 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var listOfWords = ["buccaneer", "swift", "glorious",     "incandescent", "bug", "program"]
+    var incorectMovesAllowed = 7
+    var totalWins = 0
+    var totalLosses = 0
+    
+    @IBOutlet var treeImageView: UIImageView!
 
+    @IBOutlet var correctWorldLabel: UILabel!
+    @IBOutlet var scoreLabel: UILabel!
+    
+    @IBOutlet var letterButtons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        newRound()
     }
-
-
+    var currentGame: Game!
+    
+    func newRound() {
+        let newWord = listOfWords.removeFirst()
+        currentGame = Game(word: newWord, incorrectMovesRemaining: incorectMovesAllowed, guessedLetters: [])
+        updateUI()
+    }
+    
+    func updateUI() {
+        var letters = [String]()
+        for letter in currentGame.formattedWord {
+            letters.append(String(letter))
+        }
+        let wordWithSpacing = letters.joined(separator: " ")
+        
+        correctWorldLabel.text = wordWithSpacing
+        scoreLabel.text = "Wins: \(totalWins). Losses: \(totalLosses)."
+        treeImageView.image = UIImage(named: "Tree.\(currentGame.incorrectMovesRemaining)")
+    }
+    @IBAction func letterButtonPressed(_ sender: UIButton) {
+        sender.isEnabled = false
+        let letterString = sender.configuration!.title!
+        let letter = Character(letterString.lowercased())
+        currentGame.playerGuessed(letter: letter)
+        updateUI()
+    }
+    
 }
 
