dddf146f0cf5d29a5ff5f720429735b663b1a23e
Samuel Bradshaw <sam@Samuels-MacBook-Pro.local>
2025-03-06 10:20:09 -0700

    uhhh take two on that last commit.finished social media app and scene lifetime app

---
+++ projects unit 2/App Event Count/App Event CountTests/App_Event_CountTests.swift
@@ -0,0 +1,17 @@
+//
+//  App_Event_CountTests.swift
+//  App Event CountTests
+//
+//  Created by Samuel Bradshaw  on 3/5/25.
+//
+
+import Testing
+@testable import App_Event_Count
+
+struct App_Event_CountTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
