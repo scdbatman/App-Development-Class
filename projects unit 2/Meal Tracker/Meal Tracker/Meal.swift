6f7323ecab462c7849c005903dac941bc4c85857
Samuel Bradshaw <sam@Samuels-MacBook-Pro.local>
2025-03-07 10:27:24 -0700

    finished Meal tracker app

---
+++ projects unit 2/Meal Tracker/Meal Tracker/Meal.swift
@@ -0,0 +1,13 @@
+//
+//  Meal.swift
+//  Meal Tracker
+//
+//  Created by Samuel Bradshaw  on 3/7/25.
+//
+
+import Foundation
+
+struct Meal {
+    var name: String
+    var food: [Food]
+}
---
+++ projects unit 2/Meal Tracker/Meal TrackerTests/Meal_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Meal_TrackerTests.swift
+//  Meal TrackerTests
+//
+//  Created by Samuel Bradshaw  on 3/7/25.
+//
+
+import Testing
+@testable import Meal_Tracker
+
+struct Meal_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
