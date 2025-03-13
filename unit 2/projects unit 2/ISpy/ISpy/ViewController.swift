165147f4549a3fb0884a61d37a0cf79ef1c7479f
Samuel Bradshaw <sam@Samuels-MacBook-Pro.local>
2025-03-13 10:29:25 -0600

    worked on ISpy project

---
+++ unit 2/projects unit 2/ISpy/ISpy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ISpy
+//
+//  Created by Samuel Bradshaw  on 3/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ unit 2/projects unit 2/ISpy/ISpyTests/ISpyTests.swift
@@ -0,0 +1,17 @@
+//
+//  ISpyTests.swift
+//  ISpyTests
+//
+//  Created by Samuel Bradshaw  on 3/13/25.
+//
+
+import Testing
+@testable import ISpy
+
+struct ISpyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
