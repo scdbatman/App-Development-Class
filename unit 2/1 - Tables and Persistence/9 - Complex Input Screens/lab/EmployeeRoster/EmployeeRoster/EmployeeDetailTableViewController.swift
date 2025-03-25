006f808651066a99f8fd3754b296460134e8947f
Samuel Bradshaw <sam@Samuels-MacBook-Pro.local>
2025-03-25 10:01:33 -0600

    employee roster works except date picker

--- unit 2/1 - Tables and Persistence/9 - Complex Input Screens/lab/EmployeeRoster/EmployeeRoster/EmployeeDetailTableViewController.swift
+++ unit 2/1 - Tables and Persistence/9 - Complex Input Screens/lab/EmployeeRoster/EmployeeRoster/EmployeeDetailTableViewController.swift
@@ -5,15 +5,35 @@ protocol EmployeeDetailTableViewControllerDelegate: AnyObject {
     func employeeDetailTableViewController(_ controller: EmployeeDetailTableViewController, didSave employee: Employee)
 }
 
-class EmployeeDetailTableViewController: UITableViewController, UITextFieldDelegate {
+class EmployeeDetailTableViewController: UITableViewController, UITextFieldDelegate, EmployeeTypeTableViewControllerDelegate {
+    func employeeTypeTableViewController(_ controller: EmployeeTypeTableViewController, didSelect employeeType: EmployeeType) {
+        self.employeeType = employeeType
+        employeeTypeLabel.text = employeeType.description
+        employeeTypeLabel.textColor = .black
+        updateSaveButtonState()
+        
+    }
+    
 
     @IBOutlet var nameTextField: UITextField!
     @IBOutlet var dobLabel: UILabel!
     @IBOutlet var employeeTypeLabel: UILabel!
     @IBOutlet var saveBarButtonItem: UIBarButtonItem!
     
+    @IBOutlet var dobPicker: UIDatePicker!
+    
     weak var delegate: EmployeeDetailTableViewControllerDelegate?
+    var employeeType: EmployeeType?
     var employee: Employee?
+    var isEditingBirthday = false {
+        didSet{
+            
+            tableView.beginUpdates()
+            tableView.endUpdates()
+        }
+    }
+    let dobPickerCellIndexPath = IndexPath(row: 2, section: 0)
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,7 +57,10 @@ class EmployeeDetailTableViewController: UITableViewController, UITextFieldDeleg
     }
     
     private func updateSaveButtonState() {
-        let shouldEnableSaveButton = nameTextField.text?.isEmpty == false
+        let hasEmployeeType = employeeTypeLabel.text?.isEmpty == false
+        let hasName = nameTextField.text?.isEmpty == false
+        let shouldEnableSaveButton = hasName && hasEmployeeType
+       
         saveBarButtonItem.isEnabled = shouldEnableSaveButton
     }
     
@@ -45,8 +68,11 @@ class EmployeeDetailTableViewController: UITableViewController, UITextFieldDeleg
         guard let name = nameTextField.text else {
             return
         }
+        guard let unwrappedEmployeeType = employeeType else{
+            return
+        }
         
-        let employee = Employee(name: name, dateOfBirth: Date(), employeeType: .exempt)
+        let employee = Employee(name: name, dateOfBirth: dobPicker.date, employeeType: unwrappedEmployeeType)
         delegate?.employeeDetailTableViewController(self, didSave: employee)
     }
     
@@ -58,4 +84,44 @@ class EmployeeDetailTableViewController: UITableViewController, UITextFieldDeleg
         updateSaveButtonState()
     }
 
+    @IBAction func dobDatePickerChanged(_ sender: Any) {
+        dobLabel.text = dobPicker.date.formatted(date: .abbreviated, time: .omitted)
+    }
+    
+    @IBSegueAction func showEmployeeTypes(_ coder: NSCoder) -> EmployeeTypeTableViewController? {
+        let employeeTypeVC = EmployeeTypeTableViewController(coder: coder)
+        employeeTypeVC?.delegate = self
+        return employeeTypeVC
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath == dobPickerCellIndexPath {
+            return 0
+        } else {
+            return UITableView.automaticDimension
+        
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath == dobPickerCellIndexPath {
+            return 200
+        }else {
+            return UITableView.automaticDimension
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        if indexPath == dobPickerCellIndexPath {
+            isEditingBirthday.toggle()
+            dobLabel.text = dobPicker.date.formatted(date: .abbreviated, time: .omitted)
+        } else {
+            return
+        }
+        
+        tableView.beginUpdates()
+        tableView.endUpdates()
+    }
+    
 }
